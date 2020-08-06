a149a0fde1094e9bd427b94e632b4a09841f4229
Tom Klaver <tom@codecapi.nl>
2020-08-06 10:04:08 +0200

    add script,fixture,test for golden master text

---
+++ test/golden-master-text.spec.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import * as fs from 'fs';
+
+describe('Golden master text', function () {
+    const expected = fs.readFileSync(__dirname+'/fixtures/golden_master.txt', 'ascii');
+
+    it('should equal the fixture', async function() {
+        // monkey patch console.log to catch the output
+        const oldLog = console.log;
+        let log = '';
+        console.log = text => log = log + (text || '') +'\n';
+        await import('./golden-master-text-test');
+        console.log = oldLog;
+
+        expect(log).to.equal(expected);
+    });
+
+});
